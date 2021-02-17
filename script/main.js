cc72033795f988d9e1adc1088db6a56982198057
Naggenda <naggendajoshua@gmail.com>
2021-02-17 23:46:48 +0300

    first commit

---
+++ script/main.js
@@ -0,0 +1,25 @@
+function myfunction(v) {
+  var success=document.getElementById('success');
+  if(success.style.display=='block')
+  {
+      success.style.display = 'none';
+  }
+
+
+  else{
+      success.style.display = 'block';
+  }
+  }
+
+  function select(v) {
+    var selection=document.getElementById('selection');
+    if(selection.style.display=='block')
+    {
+      selection.style.display = 'none';
+    }
+  
+  
+    else{
+      selection.style.display = 'block';
+    }
+    }
